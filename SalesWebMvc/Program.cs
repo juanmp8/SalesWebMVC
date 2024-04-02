909bc2ef5c27d2480619bfe388e640e734af4222
Juan <juanmendespeixoto@gmail.com>
2024-04-02 08:29:28 -0300

    CRUD Scaffolding

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesWebMvc.Data;
 namespace SalesWebMvc
 {
     public class Program
@@ -6,6 +9,8 @@ namespace SalesWebMvc
             Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args); 
+            builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
